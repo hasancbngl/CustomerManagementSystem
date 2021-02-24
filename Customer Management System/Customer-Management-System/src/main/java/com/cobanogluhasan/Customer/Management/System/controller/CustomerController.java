76863869fbc93350a7023f0cd7a19573a0b5a7e4
Hasan Çobanoğlu <hasan.cobanoglu1@outlook.com.tr>
2021-02-24 23:38:50 +0300

    add orderController

--- Customer Management System/Customer-Management-System/src/main/java/com/cobanogluhasan/Customer/Management/System/controller/CustomerController.java
+++ Customer Management System/Customer-Management-System/src/main/java/com/cobanogluhasan/Customer/Management/System/controller/CustomerController.java
@@ -2,12 +2,16 @@ package com.cobanogluhasan.Customer.Management.System.controller;
 
 import com.cobanogluhasan.Customer.Management.System.exception.ResourceNotFoundException;
 import com.cobanogluhasan.Customer.Management.System.model.Customer;
+import com.cobanogluhasan.Customer.Management.System.model.Order;
 import com.cobanogluhasan.Customer.Management.System.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping( "/api/v1/")
@@ -32,11 +36,35 @@ public class CustomerController {
     }
 
     //save customer
+    @PostMapping("customers")
     public Customer createCustomer(@RequestBody Customer customer) {
         return this.customerRepository.save(customer);
     }
-
     //update customer
+    @PutMapping("/customers/{id}")
+    public ResponseEntity<Customer> updateCustomer(
+            @PathVariable(value = "id") Long customerId,
+            @Validated @RequestBody Customer customerDetails) throws ResourceNotFoundException {
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found for that id" + customerId));
+
+        customer.setEmail(customerDetails.getEmail());
+        customer.setFullName(customerDetails.getFullName());
+        customer.setOrders(customerDetails.getOrders());
+
+        return ResponseEntity.ok(this.customerRepository.save(customer));
+    }
 
     //delete customer
+    @DeleteMapping("/customers/{id}")
+    public Map<String, Boolean> deleteCustomer(@PathVariable(value = "id") Long customerId) throws ResourceNotFoundException {
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found for that id" + customerId));
+
+        this.customerRepository.delete(customer);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
 }
---
+++ Customer Management System/Customer-Management-System/src/main/java/com/cobanogluhasan/Customer/Management/System/controller/OrderController.java
@@ -0,0 +1,71 @@
+package com.cobanogluhasan.Customer.Management.System.controller;
+
+import com.cobanogluhasan.Customer.Management.System.exception.ResourceNotFoundException;
+import com.cobanogluhasan.Customer.Management.System.model.Order;
+import com.cobanogluhasan.Customer.Management.System.repository.OrderRepository;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping( "/api/v1/")
+public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    //get orders
+    @GetMapping("orders")
+    public List<Order> getOrders() {
+        return this.orderRepository.findAll();
+    }
+
+    //get order by id
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable(value = "id") Long orderId)
+     throws ResourceNotFoundException {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found for that id" + orderId));
+        return ResponseEntity.ok().body(order);
+    }
+
+    //save order
+    @PostMapping("orders")
+    public Order createOrder(@RequestBody Order order) {
+        return this.orderRepository.save(order);
+    }
+
+    //update order
+    @PutMapping("/orders/{id}")
+    public ResponseEntity<Order> updateOrder(
+            @PathVariable(value = "id") Long orderId,
+            @Validated @RequestBody Order orderDetails) throws ResourceNotFoundException {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found for that id" + orderId));
+        order.setOrders(orderDetails.getOrders());
+        order.setAmount(orderDetails.getAmount());
+        order.setCustomer(orderDetails.getCustomer());
+        order.setDetails(orderDetails.getDetails());
+
+        return ResponseEntity.ok(this.orderRepository.save(order));
+    }
+
+    //delete order
+    @DeleteMapping("/orders/{id}")
+    public Map<String, Boolean> deleteOrder(@PathVariable(value = "id") Long orderId) throws ResourceNotFoundException {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found for that id" + orderId));
+
+        this.orderRepository.delete(order);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+}
