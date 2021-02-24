58491145e52a1af69a0bddd95c41a75ecbcd208e
Hasan Çobanoğlu <hasan.cobanoglu1@outlook.com.tr>
2021-02-24 14:42:35 +0300

    created customer manage pr

---
+++ Customer Management System/Customer-Management-System/src/test/java/com/cobanogluhasan/Customer/Management/System/CustomerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobanogluhasan.Customer.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
